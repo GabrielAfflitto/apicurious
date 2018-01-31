4d3c5eb576faa0f15cd7a20f8f7c97a0814b79e1
GabrielAfflitto <gafflitto92@gmail.com>
2018-01-31 13:28:59 -0700

    Added VCR gem
    VCR gem to help with testing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root "home#index"
 
   get '/auth/github/callback', to: 'sessions#create'
+
+  get '/dashboard', to: 'github_users#index'
+
   get 'logout', to: 'sessions#destroy'
 
 end
