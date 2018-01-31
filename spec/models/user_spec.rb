a37fd8bb8988f847d3ec812cb8b8a9ee8a945840
GabrielAfflitto <gafflitto92@gmail.com>
2018-01-30 19:23:54 -0700

    modified tests to match new attributes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,22 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it "creates or updates itself from a user hash" do
     auth = {
-      provider: "github",
       uid: "1234567",
       info: {
+        nickname: "GABE",
         email: "gabe@gmail.com",
         name: "Gabriel Afflitto",
+      },
+      credentials: {
+        token: "a3jh34jh5g34gjj4hg35jh"
       }
     }
 
     User.update_or_create(auth)
     new_user = User.first
 
-    expect(new_user.provider).to eq("github")
     expect(new_user.uid).to eq("1234567")
+    expect(new_user.nickname).to eq("GABE")
     expect(new_user.name).to eq("Gabriel Afflitto")
   end
 end
