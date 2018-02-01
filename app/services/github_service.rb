1c5b7d039a085df2240aa0a650e1802fcebf5306
GabrielAfflitto <gafflitto92@gmail.com>
2018-01-31 18:41:50 -0700

    Created POROs for abstracting logic
    Created GithubUser and GithubService class to divide responsibility into seperate classes and properly parse information from request

---
+++ app/services/github_service.rb
@@ -0,0 +1,31 @@
+class GithubService
+
+  def initialize(user)
+    @user = user
+    @conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.headers['Authorization'] = "token #{@user.token}"
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def user_info
+    response = @conn.get("/user")
+    b = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def user_starred_repos
+    response = @conn.get("/user/starred")
+    b = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def user_repos
+    response = @conn.get("/user/repos?type=owner", per_page: 100)
+    b = JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def user_organizations
+    response = @conn.get("/user/orgs")
+    b = JSON.parse(response.body, symbolize_names: true)
+  end
+
+end
