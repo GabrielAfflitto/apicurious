bb1ca6f19e0bb68c83a8ac3c5b4c77f8d79252df
GabrielAfflitto <gafflitto92@gmail.com>
2018-01-31 18:35:16 -0700

    Modified test to work with VCR

--- spec/features/user_logs_in_with_github_spec.rb
+++ spec/features/user_logs_in_with_github_spec.rb
@@ -21,14 +21,15 @@ RSpec.feature "user can log in" do
 
   context "using Github" do
     it "should allow user to log in" do
+      VCR.use_cassette("user can log in") do
+        visit root_path
 
-      visit root_path
+        expect(page).to have_content("Sign in with Github")
 
-      expect(page).to have_content("Sign in with Github")
-
-      click_link "Sign in with Github"
-      expect(page).to have_content("Gabriel Afflitto")
-      expect(page).to have_content("Logout")
+        click_link "Sign in with Github"
+        expect(page).to have_content("Gabriel Afflitto")
+        expect(page).to have_content("Logout")
+      end
     end
   end
 end
